dfbe19cff58963b05ab3ba10bbe78a586d43b767
Toshit Chawda <r58playz@gmail.com>
2024-12-11 20:46:35 -0800

    optimize day11

--- src/day11.rs
+++ src/day11.rs
@@ -1,9 +1,9 @@
-use std::{env, fs::read_to_string};
+use std::{env, fs::read_to_string, time::Instant};
 
 use anyhow::{Context, Result};
 use rustc_hash::FxHashMap;
 
-fn digits(mut x: u128) -> usize {
+fn digits(mut x: u64) -> u8 {
 	let mut size = 1;
 	while x > 9 {
 		x /= 10;
@@ -12,59 +12,62 @@ fn digits(mut x: u128) -> usize {
 	size
 }
 
-fn split(x: u128, size: usize) -> (u128, u128) {
-	let size = 10u128.pow((size / 2) as u32);
+fn split(x: u64, size: u8) -> (u64, u64) {
+	let size = 10u64.pow(u32::from(size / 2));
 	(x / size, x % size)
 }
 
-fn evolve_recurse(
-	n: u128,
+fn evolve_recurse<const MAX: usize>(
+	n: u64,
 	depth: usize,
-	target: usize,
-	cache: &mut FxHashMap<(usize, u128), u128>,
-) -> u128 {
-	if let Some(x) = cache.get(&(depth, n)) {
-		return *x;
+	cache: &mut FxHashMap<u64, [u64; MAX]>,
+) -> u64 {
+	if depth == MAX {
+		return 1;
+	}
+
+	if let Some(x) = cache.get(&n)
+		&& x[depth] != 0
+	{
+		return x[depth];
 	}
 
 	let ret;
 	let newdepth = depth + 1;
 
-	if depth == target {
-		return 1;
-	}
 	if n == 0 {
-		ret = evolve_recurse(1, newdepth, target, cache);
+		ret = evolve_recurse(1, newdepth, cache);
 	} else if let digits = digits(n)
 		&& digits % 2 == 0
 	{
 		let (l, r) = split(n, digits);
-		ret =
-			evolve_recurse(l, newdepth, target, cache) + evolve_recurse(r, newdepth, target, cache);
+		ret = evolve_recurse(l, newdepth, cache) + evolve_recurse(r, newdepth, cache);
 	} else {
-		ret = evolve_recurse(n * 2024, newdepth, target, cache);
+		ret = evolve_recurse(n * 2024, newdepth, cache);
 	}
-	cache.insert((depth, n), ret);
+
+	cache.entry(n).or_insert([0; MAX])[depth] = ret;
+
 	ret
 }
 
-pub fn part1(data: &[u128]) -> Result<usize> {
+pub fn part1(data: &[u64]) -> Result<u64> {
 	let mut cache = FxHashMap::default();
 	Ok(data
 		.iter()
-		.map(|x| evolve_recurse(*x, 0, 25, &mut cache) as usize)
+		.map(|x| evolve_recurse::<25>(*x, 0, &mut cache))
 		.sum())
 }
 
-pub fn part2(data: &[u128]) -> Result<usize> {
+pub fn part2(data: &[u64]) -> Result<u64> {
 	let mut cache = FxHashMap::default();
 	Ok(data
 		.iter()
-		.map(|x| evolve_recurse(*x, 0, 75, &mut cache) as usize)
+		.map(|x| evolve_recurse::<75>(*x, 0, &mut cache))
 		.sum())
 }
 
-pub fn parse(input: &str) -> Result<Vec<u128>> {
+pub fn parse(input: &str) -> Result<Vec<u64>> {
 	let input = read_to_string(input).context("failed to read input")?;
 
 	input
@@ -76,9 +79,15 @@ pub fn parse(input: &str) -> Result<Vec<u128>> {
 
 pub fn main() -> Result<()> {
 	let input = env::args().nth(1).context("no input")?;
+	let before = Instant::now();
 	let data = parse(&input)?;
+	dbg!(before.elapsed());
 
+	let before = Instant::now();
 	println!("part1 {}", part1(&data)?);
+	dbg!(before.elapsed());
+	let before = Instant::now();
 	println!("part2 {}", part2(&data)?);
+	dbg!(before.elapsed());
 	Ok(())
 }
